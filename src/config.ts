c30a0f7671ead59a4ad97ab2e6a32af167db273e
Herman S. <dreamscache.d@gmail.com>
2025-11-15 14:41:16 +0100

    Add Git redirect

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
+/** URL of a Git repository to redirect to from the root / route. */
+export const GIT_REPO_URL = "https://github.com/dreamscached/httpjax";
+
 /** Max size (in bytes) of TeX body content (in /render routes.) */
 export const MAX_TEX_BODY_SIZE = 1024;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
+import { GIT_REPO_URL } from "./config.js";
 import docs from "./routes/docs.js";
 import render from "./routes/render.js";
 
@@ -9,5 +10,6 @@ export default app;
 
 app.use(cors());
 
+app.get("/", (ctx) => ctx.redirect(GIT_REPO_URL));
 app.route("/render", render);
 app.route("/docs", docs);
